d988a0889f9c94efc21189491abf139df428848c
vitor0p9f <dev.vitorpfontes002@gmail.com>
2023-12-22 14:36:36 -0300

    feat(substrings.rb): Extending the behavior of the 'find_and_count_substrings' function
    - The function now works if the search for substrings is performed in a phrase instead of a word.
    - The function made the test 'should work with sentences' pass.

--- substrings.rb
+++ substrings.rb
@@ -3,7 +3,13 @@ def is_a_substring?(word, string)
 end
 
 def find_and_count_substrings(array, string)
-  array.each_with_object(Hash.new(0)) do |word, hash|
-    (hash[word] += 1) if is_a_substring?(word, string)
+  hash = Hash.new(0)
+
+  string.split(" ").each do |word_of_string|
+    array.each do |word|
+      (hash[word] = hash[word] + 1) if is_a_substring?(word.downcase,word_of_string.downcase)
+    end
   end
-end
+
+  hash
+end 
