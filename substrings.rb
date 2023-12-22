e0d49c2cf347a83c1b8d3ac60bed176799a94a96
vitor0p9f <dev.vitorpfontes002@gmail.com>
2023-12-22 10:01:22 -0300

    feat(substrings.rb): Creating the function that generates a hash with the substrings as the key and the number of times they appeared in the string as the value
    - The function 'find_and_count_substrings' made the test 'should check each word in an array to see if it is a substring and return a hash with the substrings found as keys and the number of times they appeared' pass.

--- substrings.rb
+++ substrings.rb
@@ -1,3 +1,9 @@
 def is_a_substring?(word, string)
   string.include?(word)
 end
+
+def find_and_count_substrings(array, string)
+  array.each_with_object(Hash.new(0)) do |word, hash|
+    (hash[word] += 1) if is_a_substring?(word, string)
+  end
+end
