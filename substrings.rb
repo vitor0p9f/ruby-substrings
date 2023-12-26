07288d6086f3fe7794468aad72f6998f29ce9f2e
vitor0p9f <dev.vitorpfontes002@gmail.com>
2023-12-26 08:36:26 -0300

    feat(substrings.rb): Adding I/O options
    - Allowing user input.
    - Displaying result on terminal.

--- substrings.rb
+++ substrings.rb
@@ -5,11 +5,29 @@ end
 def find_and_count_substrings(array, string)
   hash = Hash.new(0)
 
-  string.split(" ").each do |word_of_string|
+  string.split(' ').each do |word_of_string|
     array.each do |word|
-      (hash[word] = hash[word] + 1) if is_a_substring?(word.downcase,word_of_string.downcase)
+      (hash[word] = hash[word] + 1) if is_a_substring?(word.downcase, word_of_string.downcase)
     end
   end
 
   hash
-end 
+end
+
+print('Type a sentence: ')
+
+user_sentence = gets
+
+print("\n")
+
+print('Enter a comma-separated set of possible substrings (white spaces will be considerate part of string): ')
+
+user_set = gets
+
+print("\n")
+
+puts('Substring count:')
+
+puts "\n"
+
+puts find_and_count_substrings(user_set.gsub("\n", '').split(','), user_sentence)
